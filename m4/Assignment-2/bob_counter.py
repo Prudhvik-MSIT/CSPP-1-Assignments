73df8dbe37fdcea6f4243ced1457c15ed2af777c
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-06 17:37:11 +0530

    " -> 5 of 5 passed. pylint: 8.89/10 "

--- m4/Assignment-2/bob_counter.py
+++ m4/Assignment-2/bob_counter.py
@@ -1,21 +1,13 @@
-'''Assume s is a string of lower case characters.
-
-Write a program that prints the number of times the string 'bob'
-occurs in s. For example, if s = 'azcbobobegghakl', then your
-program should print
-
-Number of times bob occurs is: 2'''
-
+"""A program that prints the number of times the
+string 'bob' occurs in s"""
 def main():
-    """
-    main function
-    """
-    str_input = input()
-    count = 0
-    for i in range(len(str_input)-3):
-        if str_input[i:i+3] == "bob":
-            count += 1
-    print(count)
+    """ This is the code for M4-A2"""
+    str_inp = input()
+    cnt_inp = 0
+    for ind_inp in range(len(str_inp)-1):
+        if len(str_inp[ind_inp:ind_inp+3]) == 3 and str_inp[ind_inp:ind_inp+3] in 'bob':
+            cnt_inp += 1
+    print(str(cnt_inp))
 
 if __name__ == "__main__":
     main()

0862140c7cbc18513de7e67e8040ff28d52c6237
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-07 17:28:03 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -3,10 +3,10 @@ Author: Prudhvik Chirunomula
 Date: 07-08-2018
 '''
 # Exercise: Assignment-1
-# Write a Python function, factorial_val(n), that takes in one number and returns the factorial of given number.
+# Write a Python function, factorial_val(n), that takes in one number
+# and returns the factorial of given number.
 
 # This function takes in one number and returns one number.
-
 
 def factorial_val(n_val):
     '''
@@ -18,13 +18,11 @@ def factorial_val(n_val):
     if n_val == 0:
         return 1
     return n_val * factorial_val(n_val-1)
-    
-
 
 def main():
     '''main function'''
     a = input()
     print(factorial_val(int(a)))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()

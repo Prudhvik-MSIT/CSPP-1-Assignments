cddb5a0736c52e2b04ad1b1634109c4491fffc45
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-08 16:48:50 +0530

    " -> 0 of 7 passed. pylint: 8.46/10 "

---
+++ m6/p3/digit_product.py
@@ -0,0 +1,18 @@
+"""Given a  number int_input, find the product of all the digits"""
+def main():
+    '''
+    Read any number from the input, store it in variable int_input.
+    '''
+    n_inp = int(input())
+    res_out = 1
+    neg_flag = 1
+    if n_inp>0:
+        neg_flag = -1
+        n_inp *= neg_flag
+    while n_inp != 0:
+        res_out *= n_inp%10
+        n_inp //= 10
+    print(res_out*neg_flag)
+
+if __name__ == "__main__":
+    main()

66a84cec344322e334860b520c82b8c432b65fc8
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-08 16:33:26 +0530

    " -> 2 of 7 passed. pylint: 9.33/10 "

---
+++ m6/p1/fizz_buzz.py
@@ -0,0 +1,25 @@
+'''
+Write a short program that prints each number from 1 to num on a new line.
+For each multiple of 3, print "Fizz" instead of the number.
+For each multiple of 5, print "Buzz" instead of the number.
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+'''
+def main():
+    '''
+    Read number from the input, store it in variable num.
+    '''
+NUM = int(input())
+i = 1
+while i <= NUM:
+    if (i%3 == 0 and i%5 == 0):
+        print("fizz")
+        print("buzz")
+    elif i%3 == 0:
+        print("fizz")
+    elif i%5 == 0:
+        print("buzz")
+    else:
+        print(i)
+    i = i+1
+if __name__ == "__main__":
+    main()

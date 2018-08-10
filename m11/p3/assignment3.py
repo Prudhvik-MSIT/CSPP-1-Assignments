0d8b271afea0700ee197dc2b85247abd64ceb556
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-10 15:29:10 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -31,6 +31,7 @@ def is_valid_word(word_input, hand_dict, words_list):
     # TO DO ... <-- Remove this comment when you code this function
     char_count = 0
     if word_input in words_list:
+    	print(word_input, hand_dict, words_list)
 	    for each_char in word_input:
     		if each_char in hand_dict:
     			char_count += 1

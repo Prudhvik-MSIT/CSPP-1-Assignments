0f18654ba73cff99a0c15b4d67f4f8d76c9f4d0a
Prudhvik-MSIT <prudhvik.1996@msitprogram.net>
2018-08-11 18:22:40 +0530

    " -> 0 of 3 passed. pylint: 9.52/10 "

--- m12/p1/create_social_network.py
+++ m12/p1/create_social_network.py
@@ -37,16 +37,17 @@ def create_social_network(data):
     data = data.split("\n")
     # print(data)
     for each_line in data:
-        each_line = each_line.split(" follows ")
-        print(each_line)
-        key = each_line[0]
-        value = each_line[1]
-        # print(each_line[1])
-        # if "," in each_line[1]:
-        #     value = each_line[1].split(",")
-        # else:
-        #     value = [each_line[1]]
-        social_network[key] = value
+        if each_line != "":
+            each_line = each_line.split(" follows ")
+            print(each_line)
+            key = each_line[0]
+            value = each_line[1]
+            # print(each_line[1])
+            # if "," in each_line[1]:
+            #     value = each_line[1].split(",")
+            # else:
+            #     value = [each_line[1]]
+            social_network[key] = value
     return social_network
     
 
